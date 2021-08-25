046ae0a443a283febedcb2083e7693e933f43210
NoahTN <ntolentino-nguyen@csumb.edu>
2021-08-25 16:25:54 -0700

    Fix deployment directory paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     disableStaticImages: true,
   },
+    
   // your config for other plugins or the general next.js here...
 });
