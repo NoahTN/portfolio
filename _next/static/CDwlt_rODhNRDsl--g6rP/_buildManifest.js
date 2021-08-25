221896b2a85e92a5502b6de3b12e7eafdcaef778
NoahTN <42818569+NoahTN@users.noreply.github.com>
2021-08-25 23:18:55 +0000

    Deploying to gh-pages from @ NoahTN/portfolio@b51200adf8daeea4492b93b16fd42f8d088bf478 🚀

---
+++ _next/static/CDwlt_rODhNRDsl--g6rP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-f0a7ca9a2c18bf640550.js","static/css/78aca13c43798487b0dc.css","static/chunks/pages/index-599090e8bf79581b76dd.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
